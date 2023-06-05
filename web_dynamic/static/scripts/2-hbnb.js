2ac988df313abd9bdd15ca6a645a127805346a00
ndiritu-prisca <priscandiritu@outlook.com>
2023-06-05 19:38:56 +0000

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  const selectedAmenities = [];
+
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if (this.checked) {
+      selectedAmenities.push(amenityName);
+    } else {
+      const index = selectedAmenities.indexOf(amenityName);
+      if (index > -1) {
+        selectedAmenities.splice(index, 1);
+      }
+    }
+
+    updateAmenitiesText();
+  });
+
+  function updateAmenitiesText () {
+    const amenitiesText = selectedAmenities.join(', ');
+    $('.amenities h4').text(amenitiesText);
+  }
+
+  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+    if (data.status === "OK") {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
